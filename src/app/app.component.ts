159fd2ca8d0a480941d8ed5f4a50d84868a4a164
migue8104 <miguelangeljaramilloarenas6@gmail.com>
2024-09-27 00:39:22 -0500

    creacion de todo el proyecto con angular y conexion con el backend se puso hacer las listas en los paises y los estados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EstadosService } from './services/estados/estados.service';
+import { PaisesService } from './services/paises/paises.service';
+import { PersonaService } from './services/persona/persona.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'PersonaFRONTEND';
+export class AppComponent implements OnInit {
+
+  personaForm: FormGroup;
+  paises: any;
+  estados: any;
+
+  constructor(
+    public fb: FormBuilder,
+    public estadosService: EstadosService,
+    public paisesService: PaisesService,
+    public personaService: PersonaService,
+
+
+  ) {
+    this.personaForm = this.fb.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      edad: ['', Validators.required],
+      pais: ['', Validators.required],
+      estado: ['', Validators.required],
+    })
+
+  }
+  ngOnInit(): void {
+    this.paisesService.getAllPaises().subscribe(resp => {
+      this.paises = resp;
+    },
+      error => { console.error(error) }
+    )
+
+
+  }
+
+  guardar(): void {
+
+  }
+
+  cargarEstadosPorPaisesId(event: any){
+    this.estadosService.getAllEstadosByPais(event.target.value).subscribe(resp=>{
+      this.estados = resp
+    },
+    error => { console.error(error) }
+  )
+
+  }
 }
